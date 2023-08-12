3ceef2549fdeac6973c9d05ba3bf7f9b8331210f
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2023-08-12 10:05:59 -0400

    MMMCheeseJS

--- JS/app.js
+++ JS/app.js
@@ -2,14 +2,16 @@
 const buscar = document.querySelector('input.buscar#inputSearch')
 const cantidadProd = document.querySelector('span.itemCarrito')
 const main = document.querySelector('div.principal#principal')
-
+const logo = document.querySelector('img.imglogo')
 
 /*                      Funciones                      */
 
 
-/* function unidadCarro(){
-    cantidadProd.innerText = carrito.length
-} */
+function unidadCarro(){
+    cantidadProd.innerHTML = carrito.length
+}
+
+carrito.length > 0 && unidadCarro()
 
 function mostrarProductos( {id,img,nombre,precio,origen} = queso) {
     return `  <div class="card">
@@ -35,7 +37,7 @@ function cargarProductos(array){
     if(array.length > 0 ){
         array.forEach((queso) => {
             main.innerHTML += mostrarProductos(queso)
-            
+            activarClickBotones ()
         })
     }else{
         alert ('No hay productos')
@@ -47,10 +49,18 @@ function activarClickBotones (){
     botones.forEach((boton) =>{
         boton.addEventListener('click' , ()=>{
             const addProd = quesos.find((queso) => queso.id === parseInt((boton.id)))
-            console.log(addProd)
+            carrito.push(addProd)
+            unidadCarro()
         })
     })
 }
+
+
+
+/*                      Funciones                      */
+
+logo.addEventListener('mouseover', ()=> logo.title = 'Ir al inicio' )
+
 
 
 cargarProductos(quesos)
--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,8 @@ const quesos = [{id: 1, img: 'img/gouda.png', nombre: 'Gouda', precio: 5990, ori
                 {id: 2, img: 'img/edam.png', nombre: 'Edam', precio: 9990, origen: 'Países Bajos'},
                 {id: 3, img: 'img/maasdam.png', nombre: 'Maasdam', precio: 10990, origen: 'Países Bajos'},
                 {id: 4, img: 'img/brie.png', nombre: 'Brie', precio: 15990, origen: 'Francia'},
-                {id: 5, img: 'img/camembert.png', nombre: 'Camembert', precio: 18990, origen: 'Francia'},
-                {id: 6, img: 'img/roquefort.png', nombre: 'Roquefort', precio: 23990, origen: 'Francia'},
+                {id: 5, img: 'img/camembert.png', nombre: 'Camembert', precio: 18990, origen: 'Francia'}]
+/*                 {id: 6, img: 'img/roquefort.png', nombre: 'Roquefort', precio: 23990, origen: 'Francia'},
                 {id: 7, img: 'img/tulum.png', nombre: 'Tulum', precio: 14990, origen: 'Turco'},
                 {id: 8, img: 'img/emmental.png', nombre: 'Emmental', precio: 9990, origen: 'Suizo'},
                 {id: 9, img: 'img/appenzeller.png', nombre: 'Appenzeller', precio: 15990, origen: 'Suizo'},
@@ -17,7 +17,9 @@ const quesos = [{id: 1, img: 'img/gouda.png', nombre: 'Gouda', precio: 5990, ori
                 {id: 17, img: 'img/provolone.png', nombre: 'Provolone', precio: 19990, origen: 'Italiano'},
                 {id: 18, img: 'img/cabrales.png', nombre: 'Cabrales', precio: 10990, origen: 'Español'},
                 {id: 19, img: 'img/manchego.png', nombre: 'Manchego', precio: 14990, origen: 'Español'},
-                {id: 20, img: 'img/mahones.png', nombre: 'Mahonés', precio: 14990, origen: 'Español'}]
+                {id: 20, img: 'img/mahones.png', nombre: 'Mahonés', precio: 14990, origen: 'Español'}] */
 
-const carrito = []
+const carrito = [{id: 7, img: 'img/tulum.png', nombre: 'Tulum', precio: 14990, origen: 'Turco'},
+{id: 8, img: 'img/emmental.png', nombre: 'Emmental', precio: 9990, origen: 'Suizo'},
+{id: 9, img: 'img/appenzeller.png', nombre: 'Appenzeller', precio: 15990, origen: 'Suizo'}]
 
